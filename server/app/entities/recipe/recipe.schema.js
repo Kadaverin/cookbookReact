eecae0527440b45e0f1f355041d97f949724b52e
Danil Davydov <Linadakill@gmail.com>
2018-07-25 15:33:53 +0300

    add rating to Recipe model on backend

--- server/app/entities/recipe/recipe.schema.js
+++ server/app/entities/recipe/recipe.schema.js
@@ -4,7 +4,13 @@ const Recipe = new mongoose.Schema({
 	title: String,
 	description: String,
 	createdAt: String,
-	updatedAt: String
+	updatedAt: String,
+	rating: {
+		type: Number,
+		default : 0,
+		min : 0,
+		max : 5
+	}
 }, {
 	versionKey: false
 });
